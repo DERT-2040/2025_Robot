6c45563a562cb720a90d40bca98a4a378e0d4f06
ande2076 <randy.t.anderson@gmail.com>
2025-01-21 18:25:50 -0600

    Updated the PI controller limits

--- simulink/Data/Elevator.m
+++ simulink/Data/Elevator.m
@@ -2,9 +2,9 @@ Elevator_Gain_Prop = 1/20;  % DC/inch
 Elevator_Gain_Int = 0.001;  % DC/(inch*loops)
 Elevator_Int_IC = 0;
 Elevator_Int_UL = 0.1;
-Elevator_Int_LL = 0;
+Elevator_Int_LL = -0.1;
 Elevator_Total_UL = 0.2;    % DC
-Elevator_Total_LL = 0;      % DC
+Elevator_Total_LL = -0.2;      % DC
 
 elevator_sprocket_radius = 1;  % inches
 elevator_gear_ratio = 20;
