e8e38fa653b2078b82de8621b4cf58f3d16d5abc
MelainaHumbles <melhumbles@dunlapstudents.net>
2025-03-11 20:36:54 -0500

    Adding remaining paths to 3D matrix
    Adding remaining steps in State Flow for Auto
    Adding logic in SimuLink to State Flow for determining which path to use for each Path_ID

--- simulink/Data/Reefscape_Auto_Paths.m
+++ simulink/Data/Reefscape_Auto_Paths.m
@@ -1,11 +1,17 @@
 
 D2R = pi/180;
 
+% Field Size 317 in. x 691 in.
+width = 317 * 0.0254;
+length = 691 * .0254;
+
+% [x, y, velocity, angle]
+
 % Blue
 % Start:  Starting Line on Left
 % Finish: Reef Face I-J
 Path_Blue_Left_A = [
-    7.564,  6.512,  0.5,  -180    *D2R;
+    7.564,  6.512,  0.5,  -180  *D2R;
     6.652,  6.401,  1,  -167    *D2R;
     5.985,  6.140,  1,  -145    *D2R;
     5.418,  5.613,  0,  -120    *D2R;
@@ -46,15 +52,304 @@ Path_Blue_Left_C = [
     0,0,0,0;
     0,0,0,0];
 
+% Blue
+% Start:  Reef Face L-K
+% Finish: Coral Left
+Path_Blue_Left_D = [
+    3.610,  5.551,  0.5, -60    *D2R;
+    3.171,  6.130,  1,   -60    *D2R;
+    2.129,  6.419,  1,   -57    *D2R;
+    1.256,  6.872,  0,   -54    *D2R;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0];
+
+% Blue
+% Start:  Starting Line on Right
+% Finish: Reef Face E-F
+Path_Blue_Right_A = [
+    Path_Blue_Left_A(1,1),  width-Path_Blue_Left_A(1,2),  Path_Blue_Left_A(1,3),  -Path_Blue_Left_A(1,4);
+    Path_Blue_Left_A(2,1),  width-Path_Blue_Left_A(2,2),  Path_Blue_Left_A(2,3),  -Path_Blue_Left_A(2,4);
+    Path_Blue_Left_A(3,1),  width-Path_Blue_Left_A(3,2),  Path_Blue_Left_A(3,3),  -Path_Blue_Left_A(3,4);
+    Path_Blue_Left_A(4,1),  width-Path_Blue_Left_A(4,2),  Path_Blue_Left_A(4,3),  -Path_Blue_Left_A(4,4);
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0];
+
+% Blue
+% Start:  Reef Face E-F
+% Finish: Coral Right
+Path_Blue_Right_B = [
+    Path_Blue_Left_B(1,1),  width-Path_Blue_Left_B(1,2),  Path_Blue_Left_B(1,3),  -Path_Blue_Left_B(1,4);
+    Path_Blue_Left_B(2,1),  width-Path_Blue_Left_B(2,2),  Path_Blue_Left_B(2,3),  -Path_Blue_Left_B(2,4);
+    Path_Blue_Left_B(3,1),  width-Path_Blue_Left_B(3,2),  Path_Blue_Left_B(3,3),  -Path_Blue_Left_B(3,4);
+    Path_Blue_Left_B(4,1),  width-Path_Blue_Left_B(4,2),  Path_Blue_Left_B(4,3),  -Path_Blue_Left_B(4,4);
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0];
+
+% Blue
+% Start:  Coral Right
+% Finish: Reef Face C-D
+Path_Blue_Right_C = [
+    Path_Blue_Left_C(1,1),  width-Path_Blue_Left_C(1,2),  Path_Blue_Left_C(1,3),  -Path_Blue_Left_C(1,4);
+    Path_Blue_Left_C(2,1),  width-Path_Blue_Left_C(2,2),  Path_Blue_Left_C(2,3),  -Path_Blue_Left_C(2,4);
+    Path_Blue_Left_C(3,1),  width-Path_Blue_Left_C(3,2),  Path_Blue_Left_C(3,3),  -Path_Blue_Left_C(3,4);
+    Path_Blue_Left_C(4,1),  width-Path_Blue_Left_C(4,2),  Path_Blue_Left_C(4,3),  -Path_Blue_Left_C(4,4);
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0];
+
+% Blue
+% Start:  Reef Face C-D
+% Finish: Coral Right
+Path_Blue_Right_D = [
+    Path_Blue_Left_D(1,1),  width-Path_Blue_Left_D(1,2),  Path_Blue_Left_D(1,3),  -Path_Blue_Left_D(1,4);
+    Path_Blue_Left_D(2,1),  width-Path_Blue_Left_D(2,2),  Path_Blue_Left_D(2,3),  -Path_Blue_Left_D(2,4);
+    Path_Blue_Left_D(3,1),  width-Path_Blue_Left_D(3,2),  Path_Blue_Left_D(3,3),  -Path_Blue_Left_D(3,4);
+    Path_Blue_Left_D(4,1),  width-Path_Blue_Left_D(4,2),  Path_Blue_Left_D(4,3),  -Path_Blue_Left_D(4,4);
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0];
+
+% Blue
+% Start:  Starting Line on Center
+% Finish: Reef Face G-H
+Path_Blue_Center_A = [
+    7.533,  width/2,  0.5, -180    *D2R;
+    6.225,  width/2,  0,   -180    *D2R;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0];
+
+% Blue
+% Start:  Reef Face G-H
+% Finish: Starting Line on Center
+Path_Blue_Center_B = [
+    6.225,  width/2,  0.5, -180    *D2R;
+    7.046,  4.112,  1,  -180    *D2R;
+    7.504,  4.969,  1,  -90    *D2R;
+    7.504,  6.705,  0,  0    *D2R;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0];
+
+% Red
+% Start:  Starting Line on Right
+% Finish: Reef Face E-F
+Path_Red_Right_A = [
+    length-Path_Blue_Left_A(1,1),  Path_Blue_Left_A(1,2),  Path_Blue_Left_A(1,3),  -(Path_Blue_Left_A(1,4)+pi);
+    length-Path_Blue_Left_A(2,1),  Path_Blue_Left_A(2,2),  Path_Blue_Left_A(2,3),  -(Path_Blue_Left_A(2,4)+pi);
+    length-Path_Blue_Left_A(3,1),  Path_Blue_Left_A(3,2),  Path_Blue_Left_A(3,3),  -(Path_Blue_Left_A(3,4)+pi);
+    length-Path_Blue_Left_A(4,1),  Path_Blue_Left_A(4,2),  Path_Blue_Left_A(4,3),  -(Path_Blue_Left_A(4,4)+pi);
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0];
+
+% Red
+% Start:  Reef Face E-F
+% Finish: Coral Right
+Path_Red_Right_B = [
+    length-Path_Blue_Left_B(1,1),  Path_Blue_Left_B(1,2),  Path_Blue_Left_B(1,3),  -(Path_Blue_Left_B(1,4)+pi);
+    length-Path_Blue_Left_B(2,1),  Path_Blue_Left_B(2,2),  Path_Blue_Left_B(2,3),  -(Path_Blue_Left_B(2,4)+pi);
+    length-Path_Blue_Left_B(3,1),  Path_Blue_Left_B(3,2),  Path_Blue_Left_B(3,3),  -(Path_Blue_Left_B(3,4)+pi);
+    length-Path_Blue_Left_B(4,1),  Path_Blue_Left_B(4,2),  Path_Blue_Left_B(4,3),  -(Path_Blue_Left_B(4,4)+pi);
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0];
+
+% Red
+% Start:  Coral Right
+% Finish: Reef Face E-F
+Path_Red_Right_C = [
+    length-Path_Blue_Left_C(1,1),  Path_Blue_Left_C(1,2),  Path_Blue_Left_C(1,3),  -(Path_Blue_Left_C(1,4)+pi);
+    length-Path_Blue_Left_C(2,1),  Path_Blue_Left_C(2,2),  Path_Blue_Left_C(2,3),  -(Path_Blue_Left_C(2,4)+pi);
+    length-Path_Blue_Left_C(3,1),  Path_Blue_Left_C(3,2),  Path_Blue_Left_C(3,3),  -(Path_Blue_Left_C(3,4)+pi);
+    length-Path_Blue_Left_C(4,1),  Path_Blue_Left_C(4,2),  Path_Blue_Left_C(4,3),  -(Path_Blue_Left_C(4,4)+pi);
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0];
+
+% Red
+% Start:  Reef Face E-F
+% Finish: Coral Right
+Path_Red_Right_D = [
+    length-Path_Blue_Left_D(1,1),  Path_Blue_Left_D(1,2),  Path_Blue_Left_D(1,3),  -(Path_Blue_Left_D(1,4)+pi);
+    length-Path_Blue_Left_D(2,1),  Path_Blue_Left_D(2,2),  Path_Blue_Left_D(2,3),  -(Path_Blue_Left_D(2,4)+pi);
+    length-Path_Blue_Left_D(3,1),  Path_Blue_Left_D(3,2),  Path_Blue_Left_D(3,3),  -(Path_Blue_Left_D(3,4)+pi);
+    length-Path_Blue_Left_D(4,1),  Path_Blue_Left_D(4,2),  Path_Blue_Left_D(4,3),  -(Path_Blue_Left_D(4,4)+pi);
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0];
+
+% Red
+% Start:  Starting Line on Left
+% Finish: Reef Face E-F
+Path_Red_Left_A = [
+    length-Path_Blue_Right_A(1,1),  Path_Blue_Right_A(1,2),  Path_Blue_Right_A(1,3),  -(Path_Blue_Right_A(1,4)+pi);
+    length-Path_Blue_Right_A(2,1),  Path_Blue_Right_A(2,2),  Path_Blue_Right_A(2,3),  -(Path_Blue_Right_A(2,4)+pi);
+    length-Path_Blue_Right_A(3,1),  Path_Blue_Right_A(3,2),  Path_Blue_Right_A(3,3),  -(Path_Blue_Right_A(3,4)+pi);
+    length-Path_Blue_Right_A(4,1),  Path_Blue_Right_A(4,2),  Path_Blue_Right_A(4,3),  -(Path_Blue_Right_A(4,4)+pi);
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0];
+
+% Red
+% Start:  Reef Face E-F
+% Finish: Coral Left
+Path_Red_Left_B = [
+    length-Path_Blue_Right_B(1,1),  Path_Blue_Right_B(1,2),  Path_Blue_Right_B(1,3),  -(Path_Blue_Right_B(1,4)+pi);
+    length-Path_Blue_Right_B(2,1),  Path_Blue_Right_B(2,2),  Path_Blue_Right_B(2,3),  -(Path_Blue_Right_B(2,4)+pi);
+    length-Path_Blue_Right_B(3,1),  Path_Blue_Right_B(3,2),  Path_Blue_Right_B(3,3),  -(Path_Blue_Right_B(3,4)+pi);
+    length-Path_Blue_Right_B(4,1),  Path_Blue_Right_B(4,2),  Path_Blue_Right_B(4,3),  -(Path_Blue_Right_B(4,4)+pi);
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0];
+
+% Red
+% Start:  Coral Left
+% Finish: Reef Face E-F
+Path_Red_Left_C = [
+    length-Path_Blue_Right_C(1,1),  Path_Blue_Right_C(1,2),  Path_Blue_Right_C(1,3),  -(Path_Blue_Right_C(1,4)+pi);
+    length-Path_Blue_Right_C(2,1),  Path_Blue_Right_C(2,2),  Path_Blue_Right_C(2,3),  -(Path_Blue_Right_C(2,4)+pi);
+    length-Path_Blue_Right_C(3,1),  Path_Blue_Right_C(3,2),  Path_Blue_Right_C(3,3),  -(Path_Blue_Right_C(3,4)+pi);
+    length-Path_Blue_Right_C(4,1),  Path_Blue_Right_C(4,2),  Path_Blue_Right_C(4,3),  -(Path_Blue_Right_C(4,4)+pi);
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0];
+
+% Red
+% Start:  Reef Face E-F
+% Finish: Coral Left
+Path_Red_Left_D = [
+    length-Path_Blue_Right_D(1,1),  Path_Blue_Right_D(1,2),  Path_Blue_Right_D(1,3),  -(Path_Blue_Right_D(1,4)+pi);
+    length-Path_Blue_Right_D(2,1),  Path_Blue_Right_D(2,2),  Path_Blue_Right_D(2,3),  -(Path_Blue_Right_D(2,4)+pi);
+    length-Path_Blue_Right_D(3,1),  Path_Blue_Right_D(3,2),  Path_Blue_Right_D(3,3),  -(Path_Blue_Right_D(3,4)+pi);
+    length-Path_Blue_Right_D(4,1),  Path_Blue_Right_D(4,2),  Path_Blue_Right_D(4,3),  -(Path_Blue_Right_D(4,4)+pi);
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0];
+
+% Red
+% Start:  Starting Line on Center
+% Finish: Reef Face G-H
+Path_Red_Center_A = [
+    length-Path_Blue_Center_A(1,1),  Path_Blue_Center_A(1,2),  Path_Blue_Center_A(1,3),  0;
+    length-Path_Blue_Center_A(2,1),  Path_Blue_Center_A(2,2),  Path_Blue_Center_A(2,3),  0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0];
+
+% Red
+% Start:  Reef Face G-H
+% Finish: Starting Line on Center
+Path_Red_Center_B = [
+    length-Path_Blue_Center_B(1,1),  width-Path_Blue_Center_B(1,2),  Path_Blue_Center_B(1,3),  -(Path_Blue_Center_B(1,4)+pi);
+    length-Path_Blue_Center_B(2,1),  width-Path_Blue_Center_B(2,2),  Path_Blue_Center_B(2,3),  -(Path_Blue_Center_B(2,4)+pi);
+    length-Path_Blue_Center_B(3,1),  width-Path_Blue_Center_B(3,2),  Path_Blue_Center_B(3,3),  -(Path_Blue_Center_B(3,4)+pi);
+    length-Path_Blue_Center_B(4,1),  width-Path_Blue_Center_B(4,2),  Path_Blue_Center_B(4,3),  -(Path_Blue_Center_B(4,4)+pi);
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0;
+    0,0,0,0];
+
 % concatenate all of the paths into one multi-dimensional matrix
 All_Paths_Ref_Poses = cat(3, ...
     Path_Blue_Left_A, ...
     Path_Blue_Left_B, ...
-    Path_Blue_Left_C);
+    Path_Blue_Left_C, ...
+    Path_Blue_Left_D, ...
+    Path_Blue_Right_A, ...
+    Path_Blue_Right_B, ...
+    Path_Blue_Right_C, ...
+    Path_Blue_Right_D, ...
+    Path_Blue_Center_A, ...
+    Path_Blue_Center_B, ...
+    Path_Red_Right_A, ...
+    Path_Red_Right_B, ...
+    Path_Red_Right_C, ...
+    Path_Red_Right_D, ...
+    Path_Red_Left_A, ...
+    Path_Red_Left_B, ...
+    Path_Red_Left_C, ...
+    Path_Red_Left_D, ...
+    Path_Red_Center_A, ...
+    Path_Red_Center_B);
 
 % collect the number of poses in each path
 All_Paths_Num_Poses = [
     nnz(Path_Blue_Left_A(:,1));
     nnz(Path_Blue_Left_B(:,1));
-    nnz(Path_Blue_Left_C(:,1))];
+    nnz(Path_Blue_Left_C(:,1));
+    nnz(Path_Blue_Left_D(:,1));
+    nnz(Path_Blue_Right_A(:,1));
+    nnz(Path_Blue_Right_B(:,1));
+    nnz(Path_Blue_Right_C(:,1));
+    nnz(Path_Blue_Right_D(:,1));
+    nnz(Path_Blue_Center_A(:,1));
+    nnz(Path_Blue_Center_B(:,1));
+    nnz(Path_Red_Left_A(:,1));
+    nnz(Path_Red_Left_B(:,1));
+    nnz(Path_Red_Left_C(:,1));
+    nnz(Path_Red_Left_D(:,1));
+    nnz(Path_Red_Right_A(:,1));
+    nnz(Path_Red_Right_B(:,1));
+    nnz(Path_Red_Right_C(:,1));
+    nnz(Path_Red_Right_D(:,1));
+    nnz(Path_Red_Center_A(:,1));
+    nnz(Path_Red_Center_B(:,1))];
 
